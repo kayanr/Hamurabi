70ce48982afea97a90a1e5cfba72ad8121556ced
Kayan Reid <kreid@Zipcoders-MBP.fios-router.home>
2024-11-11 06:37:09 -0500

    updated pom file, added additional methods to Hammurabi class.

--- src/main/java/hammurabi/Hammurabi.java
+++ src/main/java/hammurabi/Hammurabi.java
@@ -15,75 +15,133 @@ public class Hammurabi {
     void playGame() {
         // declare local variables here: grain, population, etc.
         // statements go after the declarations
-        int price;
-        int bushels;
-        int acresOwned;
-        int population;
+        int price = 19;
+        int bushels = 2800;
+        int acresOwned = 1000;
+        int population = 100;
         int bushelsFedToPeople;
-        int howManyPeopleStarved;
+        int howManyPeopleStarved = 0;
         int grainStorage;
         int bushelUsedAsSeed;
-        int year;
-        printSummary();
+        int year = 1;
+
+         do{
+            printSummary(population, howManyPeopleStarved, year, acresOwned, price);
+            int i = askHowManyAcresToBuy(price, bushels);
+             acresOwned += i;
+            int sell = 0;
+            if(i == 0){
+                sell = askHowManyAcresToSell(acresOwned);
+                acresOwned -= sell;
+            }
+             price = newCostOfLand();
+             year = year + 1;
+        }   while(year <= 3);
     }
 
     //Methods to be used in the playGame method
     int askHowManyAcresToBuy(int price, int bushels){
-        int acresToBuyResponse = getNumber("How many acres to buy?: ");
-        int bushelsNeeded = acresToBuyResponse * price;
-        int i = (bushelsNeeded < bushels) ? acresToBuyResponse : 0;
-        return i;
+        int acresToBuyResponse = 0;
+        //int bushelsNeeded  = acresToBuyResponse * price;
+        int maxAcresCanBuy = bushels / price;
+
+        int i = 0;
+
+        do{
+            System.out.println("You can buy a maximum of "+ maxAcresCanBuy+ " acres!");
+            acresToBuyResponse = getNumber("How many acres of land do you want to buy?: ");
+            if(acresToBuyResponse == 0){
+                return acresToBuyResponse;
+            }
+            if(acresToBuyResponse <= maxAcresCanBuy){
+                return acresToBuyResponse;
+            }
+            System.out.println("O great Hammurabi! You jest! Please choose again!");
+        } while(acresToBuyResponse > maxAcresCanBuy);
+
+    return acresToBuyResponse;
     }
 
     int askHowManyAcresToSell(int acresOwned){
-        int acresToSellResponse = getNumber("How many acres to sell?: ");
-        int i = (acresToSellResponse < acresOwned) ? acresToSellResponse : 0;
-        return i;
+        int acresToSellResponse = 0;
+        //int i = (acresToSellResponse < acresOwned) ? acresToSellResponse : 0;
+        do{
+            System.out.println("You can sell a maximum of "+ acresOwned+ " acres!");
+            acresToSellResponse = getNumber("How many acres of land do you want to buy?: ");
+            if(acresToSellResponse == 0){
+                return acresToSellResponse;
+            }
+            if(acresToSellResponse <= acresOwned){
+                return acresToSellResponse;
+            }
+            System.out.println("O great Hammurabi! You jest! Please choose again!");
+        } while(acresToSellResponse > acresOwned);
+        return acresToSellResponse;
+
     }
 
     int askHowMuchGrainToFeedPeople(int bushels){
-
-        return 0;
+        int grainToFeedPeopleResponse = getNumber("How much grain to feed people?: ");
+        int i = (grainToFeedPeopleResponse < bushels) ? grainToFeedPeopleResponse : 0;
+        return i;
     }
 
     int askHowManyAcresToPlant(int acresOwned, int population, int bushels){
+
         return 0;
     }
-    int plagueDeaths(int population){
+    public int plagueDeaths(int population){
         return 0;
     }
-    int starvationDeaths(int population, int bushelsFedToPeople){
+    public int starvationDeaths(int population, int bushelsFedToPeople){
         return 0;
     }
-    boolean uprising(int population, int howManyPeopleStarved){
+    public boolean uprising(int population, int howManyPeopleStarved){
         return false;
     }
-    int immigrants(int population, int acresOwned, int grainInStorage){
+    public int immigrants(int population, int acresOwned, int grainInStorage){
         return 0;
     }
     int harvest(int acres, int bushelsUsedAsSeed){
         return 0;
     }
-    int grainEatenByRats(int bushels){
-        return 0;
-    }
-    int newCostOfLand(){
+    public int grainEatenByRats(int bushels){
         return 0;
     }
 
-    void printSummary(){
+    public int newCostOfLand(){
+        Random rand = new Random();
+        int landCost =  rand.nextInt(7) + 17;
+        return landCost;
+    }
+
+    void printSummary(int population, int howManyPeopleStarved, int year, int acresOwned, int price){
+        System.out.println("*********************** YEAR "+ year+ " *************************************");
         System.out.println("O great Hammurabi!\n" +
-                "You are in year 1 of your ten year rule.\n" +
-                "In the previous year 0 people starved to death.\n" +
-                "In the previous year 5 people entered the kingdom.\n" +
-                "The population is now 100.\n" +
-                "We harvested 3000 bushels at 3 bushels per acre.\n" +
-                "Rats destroyed 200 bushels, leaving 2800 bushels in storage.\n" +
-                "The city owns 1000 acres of land.\n" +
-                "Land is currently worth 19 bushels per acre.");
-    }
-    void finalSummary(){
+                "You are in year "+ year+" of your ten year rule.\n" +
+                "In the previous year "+howManyPeopleStarved+" people starved to death.\n" +
+                "In the previous year " + 5+" people entered the kingdom.\n" +
+                "The population is now "+ population +".\n" +
+                "We harvested "+3000+" bushels at "+3+" bushels per acre.\n" +
+                "Rats destroyed "+200 +" bushels, leaving "+ 2800+" bushels in storage.\n" +
+                "The city owns "+ acresOwned+" acres of land.\n" +
+                "Land is currently worth "+ price+" bushels per acre.");
+        System.out.println("********************************************************");
 
+
+    }
+    void finalSummary(int population, int howManyPeopleStarved, int year, int acresOwned, int price){
+        System.out.println("************************************* FINAL SUMMARY *************************************");
+        System.out.println("O great Hammurabi!\n" +
+                "You are in year "+ year+" of your ten year rule.\n" +
+                "In the previous year "+howManyPeopleStarved+" people starved to death.\n" +
+                "In the previous year " + 5+" people entered the kingdom.\n" +
+                "The population is now "+ population +".\n" +
+                "We harvested "+3000+" bushels at "+3+" bushels per acre.\n" +
+                "Rats destroyed "+200 +" bushels, leaving "+ 2800+" bushels in storage.\n" +
+                "The city owns "+ acresOwned+" acres of land.\n" +
+                "Land is currently worth "+ price+" bushels per acre.");
+        System.out.println("**************************************************************************");
     }
 
     /**
